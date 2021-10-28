4e094e4279aa9aeb013f011e40b03afd09a0f935
Evgenii Grigorev <eagrigorev87@gmail.com>
2021-10-28 15:36:51 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clothing-store-template
+
 A multipage HTML template for a clothing store.
+
+Figma template: https://www.figma.com/file/P6G1VYCw2ppqNAiTxlWkZP/Clothing-Store-Template?node-id=0%3A1. I don't own that template, it was provided for free by my student project curator.
---
+++ nav.js
@@ -0,0 +1,10 @@
+let button = document.querySelector(".nav__right__burger-icon");
+let nav = document.querySelector(".nav__burger");
+let menuClose = document.querySelector(".nav__burger__cancel");
+button.onclick = () => {
+    nav.classList.toggle("nav__burger__open");
+    nav.classList.toggle("nav__burger__close");
+};
+menuClose.onclick = () => {
+    nav.classList.toggle("nav__burger__close");
+};
